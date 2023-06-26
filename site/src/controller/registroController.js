253e8e7ff9355cef0d642d28d1e41f71c5eaadb5
Caetano Resende Domingues <caetano.domingues@sptech.school>
2023-06-26 02:30:38 -0300

    Select dos paises funcioando perfeitamente

--- site/src/controller/registroController.js
+++ site/src/controller/registroController.js
@@ -1,15 +1,15 @@
 var registroModel = require("../model/registroModel");
 
 function registrar(req, res) {
-    var acertos = req.body.acertosServer;
-    var erros = req.body.errosServer;
-    var fkusuario = req.body.fkusuarioServer;
+    var acerto = req.acertosServer;
+    var erro = req.errosServer;
+    var fkusuario = req.fkusuarioServer;
 
-    if (acertos == undefined || erros == undefined || fkusuario == undefined) {
+    if (acerto == undefined || erro == undefined || fkusuario == undefined) {
         res.status(400).send("Nenhum dado registrado!");
     } else {
         
-        registroModel.registrar(acertos,erros,fkusuario)
+        registroModel.registrar(acerto,erro,fkusuario)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- site/src/model/registroModel.js
+++ site/src/model/registroModel.js
@@ -1,12 +1,12 @@
 var database = require("../database/configuracao")
 
-function registrar(acertos,erros,fkusuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function registrar():", acertos,erros,fkusuario);
+function registrar(acerto,erro,fkusuario) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function registrar():", acerto,erro,fkusuario);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO Quiz (acertos,erros,fkusuario) VALUES ('${acertos}', '${erros}','${fkusuario}' );
+        INSERT INTO Quiz (acerto,erro,fkusuario) VALUES ('${acertos}', '${erros}','${fkusuario}' );
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
